d1dfcbef94d80c8b5ba24342905488180a5c24dc
Mohammed_Ashraf <mohamadd.abdelrahim@gmail.com>
2025-01-24 19:30:24 +0200

    Added metapackage and description packages

---
+++ auto_quad_description/launch/display.launch.py
@@ -0,0 +1,54 @@
+import os
+from launch.substitutions import LaunchConfiguration
+import launch_ros
+import launch
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    
+    urdf_file_name = 'urdf/auto_quad.urdf'
+    pkgPath = launch_ros.substitutions.FindPackageShare(package='auto_quad_description').find('auto_quad_description')
+    urdf = os.path.join(pkgPath, urdf_file_name)
+    
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+    
+    params = {'robot_description': robot_desc}
+
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        output='screen',
+        parameters=[params],
+        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+    )
+
+    joint_state_publisher_gui_node = launch_ros.actions.Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+    )
+
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen'
+     )
+    
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(name='gui', default_value='True', description='This is a flag for joint_state_publisher_gui'),
+        launch.actions.DeclareLaunchArgument(name='model', default_value=urdf, description='Path to the urdf model file'),
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        rviz_node
+    ])
